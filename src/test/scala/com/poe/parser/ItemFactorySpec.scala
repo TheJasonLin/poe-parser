d0267137ccf17ec7a73e95e2ed9b1d07f6193744
Jason Lin <j@sonl.in>
2017-04-24 13:27:33 -0500

    added a tests

--- src/test/scala/com/poe/parser/ItemFactorySpec.scala
+++ src/test/scala/com/poe/parser/ItemFactorySpec.scala
@@ -3,10 +3,12 @@ package com.poe.parser
 import com.poe.constants.Rarity
 import com.poe.parser.item.Item
 import com.poe.parser.item.equipment.accessory.Ring
+import com.poe.parser.item.equipment.weapon.Dagger
 import org.scalatest.{FlatSpec, Matchers}
 
 class ItemFactorySpec extends FlatSpec with Matchers {
-  "ItemFactory" should "create a ring" in {
+
+  "create" should "create a ring" in {
     val ringInfo: KnownInfo = getGhoulTurnInfo
 
     val item: Item = ItemFactory.create(ringInfo)
@@ -18,7 +20,19 @@ class ItemFactorySpec extends FlatSpec with Matchers {
     assert(ring.explicits.size == 6)
   }
 
-  def getGhoulTurnInfo: KnownInfo = {
+  "create" should "create a dagger" in {
+    val daggerInfo = getCarrionStinger
+
+    val item = ItemFactory.create(daggerInfo)
+
+    assert(item.isInstanceOf[Dagger])
+    val dagger: Dagger = item.asInstanceOf[Dagger]
+    assert(dagger.implicits.size == 1)
+    assert(dagger.explicits.size == 6)
+    assert(dagger.itemLevel == 78)
+  }
+
+  private def getGhoulTurnInfo: KnownInfo = {
     val ringInfo: KnownInfo = new KnownInfo("Diamond Ring", Rarity.RARE)
     ringInfo.name = Option("Ghoul Turn")
     ringInfo.itemLevel = Option(77)
@@ -40,5 +54,32 @@ class ItemFactorySpec extends FlatSpec with Matchers {
     )
 
     ringInfo
+  }
+
+  private def getCarrionStinger: KnownInfo = {
+    val info: KnownInfo = new KnownInfo("Royal Skean", Rarity.RARE)
+    info.name = Option("Carrion Stinger")
+    info.itemLevel = Option(78)
+    info.identified = Option(true)
+    info.quality = Option(20)
+    info.implicits = Option(
+      List("30% increased Global Critical Strike Chance")
+    )
+    info.explicits = Option(
+      List(
+        "Adds 42 to 82 Fire Damage",
+        "Adds 5 to 91 Lightning Damage to Spells",
+        "19% increased Attack Speed",
+        "107% increased Critical Strike Chance for Spells",
+        "+23% to Global Critical Strike Multiplier",
+        "43% increased Spell Damage"
+      )
+    )
+    info.accountName = Option("foxlin")
+    info.lastCharacterName = Option("SparkTrain")
+    info.note = Option("~b/o 30 chaos")
+    info.stashName = Option("$$")
+
+    info
   }
 }
